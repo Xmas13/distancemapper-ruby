d78faf15859b69035c28ec1d900d242e7096705a
Xmas13 <redherring@protonmail.com>
2015-11-04 23:41:51 -0600

    This is the first commit for this terminal program. Just need to get my changes saved and be available to my other computers. There is no error checking so far and I'm sure there is a better way to do this, but this is mostly a program for learning some basic concepts.
    To Dos:
    Do error checking for when a user types in something that the API
    can't handle.
    Make it more flexible to allow the user to type in more addresses
    to check and print out results for each one

---
+++ README.md
@@ -0,0 +1,1 @@
+# distancemapper-ruby
---
+++ main.rb
@@ -0,0 +1,42 @@
+require 'httparty'
+Key = "AIzaSyDgj3QvkHWmtugtk2kB3BQt3QNVBD51lDs"
+Unit = "imperial"
+
+class GoogleMapper
+    include HTTParty
+    base_uri 'https://maps.googleapis.com'
+
+    def initialize(origins, destinations, units, key)
+        @options = { query: {origins: origins, destinations: destinations, units: units, key: key}}
+    end
+
+    def distance
+        self.class.get("/maps/api/distancematrix/json", @options)
+    end
+end
+
+def distance_parser(mapped_object)
+    mapped_object.distance["rows"][0]["elements"][0]["distance"]["text"]
+end
+
+Gem.win_platform? ? (system "cls") : (system "clear")
+
+puts "Welcome to the DistanceMapper(v0.1)!"
+puts ""
+
+puts "What is the origin address?"
+puts ""
+origin = gets.to_s.chomp
+puts ""
+
+puts "What is the destination address?"
+puts ""
+destination = gets.to_s.chomp
+puts ""
+
+g = GoogleMapper.new(origin, destination, Unit, Key)
+
+origin_to_destination = distance_parser(g)
+
+puts "Ok, so the distance between #{g.distance["origin_addresses"].to_s} and #{g.distance["destination_addresses"].to_s} is: #{origin_to_destination}"
+
