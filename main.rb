6ed8f18a9695bb3fd19d4dd7b175b3ba7fe43bb9
Xmas13 <redherring@protonmail.com>
2015-11-11 20:10:28 -0600

    Added error checking. Need to add more comments

--- main.rb
+++ main.rb
@@ -10,13 +10,17 @@ class DistanceMapper
         @options = { query: {origins: origins, destinations: destinations, units: units, key: key}}
     end
 
-    def distance
+    def distancematrix
         self.class.get("/maps/api/distancematrix/json", @options)
     end
 end
 
 def distance_parser(mapped_object)
-    mapped_object.distance["rows"][0]["elements"][0]["distance"]["text"]
+    mapped_object.distancematrix["rows"][0]["elements"][0]["distance"]["text"]
+end
+
+def time_parser(mapped_object)
+    mapped_object.distancematrix["rows"][0]["elements"][0]["duration"]["text"]
 end
 
 Gem.win_platform? ? (system "cls") : (system "clear")
@@ -36,6 +40,14 @@ puts ""
 
 g = DistanceMapper.new(origin, destination, Distance_Unit, Distance_Key)
 
-origin_to_destination = distance_parser(g)
+# Checks to ensure if request is valid
+abort("You did not enter both the origin and destination, please try again!") unless g.distancematrix["status"] == "OK"
 
-puts "Ok, so the distance between #{g.distance["origin_addresses"].to_s} and #{g.distance["destination_addresses"].to_s} is: #{origin_to_destination}"
+# Checks to see if it can find origin and destination can be found
+abort("Could not find either the origin or destination with information provided, please try again!") unless g.distancematrix["rows"][0]["elements"][0]["status"] == "OK"
+
+
+origin_to_destination_distance = distance_parser(g)
+origin_to_destination_time = time_parser(g)
+
+puts "Ok, so the distance between #{g.distancematrix["origin_addresses"].to_s} and #{g.distancematrix["destination_addresses"].to_s} is: #{origin_to_destination_distance}. The total time to get there is: #{origin_to_destination_time}." 
